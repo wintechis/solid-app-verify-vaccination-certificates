4267bc4072845a074728e25c9f0e905407dd0935
Yannick <wiesnery@msn.com>
2022-05-20 18:01:24 +0200

    nx generate-workspace - delete e2e tests

---
+++ apps/solvercred/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'solid-app-verifiable-credentials-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'solvercred';
+}
